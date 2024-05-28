0127c6b707175ade1dadac0d16ef36a46fa99071
Brian Shu <littlebubu.shu@gmail.com>
2024-05-28 13:33:37 -0400

    lower text requirement

--- tree-sitter-haskell/app/Main.hs
+++ tree-sitter-haskell/app/Main.hs
@@ -1,15 +1,15 @@
 {-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE ImportQualifiedPost #-}
 
+import Data.Text.IO qualified as T.IO
 import System.Environment (getArgs)
 import Text.Pretty.Simple
 import TreeSitter.Api
 import TreeSitter.Haskell
-import Data.Text.IO.Utf8 qualified as T.IO.Utf8
 
 main :: IO ()
 main = do
   [file] <- getArgs
-  !tree <- parse tree_sitter_haskell <$> T.IO.Utf8.readFile file
+  !tree <- parse tree_sitter_haskell <$> T.IO.readFile file
   pPrintOpt NoCheckColorTty (defaultOutputOptionsDarkBg {outputOptionsIndentAmount = 1}) tree
   pure ()
