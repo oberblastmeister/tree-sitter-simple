323ee6cdd9c149ef59fca393ab28e06aba00640c
Brian Shu <littlebubu.shu@gmail.com>
2024-06-03 12:47:24 -0400

    format

--- tree-sitter-simple/src/TreeSitter/Raw/Node.hs
+++ tree-sitter-simple/src/TreeSitter/Raw/Node.hs
@@ -1,15 +1,20 @@
-{-# LANGUAGE DeriveGeneric, GeneralizedNewtypeDeriving, RankNTypes, ScopedTypeVariables #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# OPTIONS_GHC -funbox-strict-fields #-}
+
 module TreeSitter.Raw.Node
-( Node(..)
-, nodeStartPoint
-, nodeStartByte
-, TSPoint(..)
-, TSNode(..)
-, FieldId(..)
-, ts_node_copy_child_nodes
-, ts_node_poke_p
-) where
+  ( Node (..),
+    nodeStartPoint,
+    nodeStartByte,
+    TSPoint (..),
+    TSNode (..),
+    FieldId (..),
+    ts_node_copy_child_nodes,
+    ts_node_poke_p,
+  )
+where
 
 import Foreign
 import Foreign.C
@@ -17,15 +22,15 @@ import GHC.Generics
 import TreeSitter.Raw.Symbol (TSSymbol)
 
 data Node = Node
-  { nodeTSNode     :: !TSNode
-  , nodeType       :: !CString
-  , nodeSymbol     :: !TSSymbol
-  , nodeEndPoint   :: !TSPoint
-  , nodeEndByte    :: !Word32
-  , nodeChildCount :: !Word32
-  , nodeFieldName  :: !CString
-  , nodeIsNamed    :: !CBool
-  , nodeIsExtra    :: !CBool
+  { nodeTSNode :: !TSNode,
+    nodeType :: !CString,
+    nodeSymbol :: !TSSymbol,
+    nodeEndPoint :: !TSPoint,
+    nodeEndByte :: !Word32,
+    nodeChildCount :: !Word32,
+    nodeFieldName :: !CString,
+    nodeIsNamed :: !CBool,
+    nodeIsExtra :: !CBool
   }
   deriving (Show, Eq, Generic)
 
@@ -35,52 +40,59 @@ nodeStartPoint node = let TSNode _ p _ _ _ = nodeTSNode node in p
 nodeStartByte :: Node -> Word32
 nodeStartByte node = let TSNode b _ _ _ _ = nodeTSNode node in b
 
-data TSPoint = TSPoint { pointRow :: !Word32, pointColumn :: !Word32 }
+data TSPoint = TSPoint {pointRow :: !Word32, pointColumn :: !Word32}
   deriving (Show, Eq, Generic)
 
 data TSNode = TSNode !Word32 !TSPoint !Word32 !(Ptr ()) !(Ptr ())
   deriving (Show, Eq, Generic)
 
-newtype FieldId = FieldId { getFieldId :: Word16 }
+newtype FieldId = FieldId {getFieldId :: Word16}
   deriving (Eq, Ord, Show, Storable)
 
-
 -- | 'Struct' is a strict 'Monad' with automatic alignment & advancing, & inferred type.
-newtype Struct a = Struct { runStruct :: forall b . Ptr b -> IO (a, Ptr a) }
+newtype Struct a = Struct {runStruct :: forall b. Ptr b -> IO (a, Ptr a)}
 
 evalStruct :: Struct a -> Ptr b -> IO a
 evalStruct s p = fmap fst $! runStruct s p
 {-# INLINE evalStruct #-}
 
-peekStruct :: forall a . Storable a => Struct a
-peekStruct = Struct (\ p -> do
-  let aligned = alignPtr (castPtr p) (alignment (undefined :: a))
-  a <- peek aligned
-  pure (a, aligned `plusPtr` sizeOf a))
+peekStruct :: forall a. (Storable a) => Struct a
+peekStruct =
+  Struct
+    ( \p -> do
+        let aligned = alignPtr (castPtr p) (alignment (undefined :: a))
+        a <- peek aligned
+        pure (a, aligned `plusPtr` sizeOf a)
+    )
 {-# INLINE peekStruct #-}
 
-pokeStruct :: Storable a => a -> Struct ()
-pokeStruct a = Struct (\ p -> do
-  let aligned = alignPtr (castPtr p) (alignment a)
-  poke aligned a
-  pure ((), castPtr aligned `plusPtr` sizeOf a))
+pokeStruct :: (Storable a) => a -> Struct ()
+pokeStruct a =
+  Struct
+    ( \p -> do
+        let aligned = alignPtr (castPtr p) (alignment a)
+        poke aligned a
+        pure ((), castPtr aligned `plusPtr` sizeOf a)
+    )
 {-# INLINE pokeStruct #-}
-
 
 instance Storable Node where
   alignment _ = alignment (undefined :: TSNode)
   {-# INLINE alignment #-}
   sizeOf _ = 80
   {-# INLINE sizeOf #-}
-  peek = evalStruct $ Node <$> peekStruct
-                           <*> peekStruct
-                           <*> peekStruct
-                           <*> peekStruct
-                           <*> peekStruct
-                           <*> peekStruct
-                           <*> peekStruct
-                           <*> peekStruct
-                           <*> peekStruct
+  peek =
+    evalStruct $
+      Node
+        <$> peekStruct
+        <*> peekStruct
+        <*> peekStruct
+        <*> peekStruct
+        <*> peekStruct
+        <*> peekStruct
+        <*> peekStruct
+        <*> peekStruct
+        <*> peekStruct
   {-# INLINE peek #-}
   poke ptr (Node n t s ep eb c fn nn ne) = flip evalStruct ptr $ do
     pokeStruct n
@@ -99,8 +111,11 @@ instance Storable TSPoint where
   {-# INLINE alignment #-}
   sizeOf _ = 8
   {-# INLINE sizeOf #-}
-  peek = evalStruct $ TSPoint <$> peekStruct
-                              <*> peekStruct
+  peek =
+    evalStruct $
+      TSPoint
+        <$> peekStruct
+        <*> peekStruct
   {-# INLINE peek #-}
   poke ptr (TSPoint r c) = flip evalStruct ptr $ do
     pokeStruct r
@@ -112,11 +127,14 @@ instance Storable TSNode where
   {-# INLINE alignment #-}
   sizeOf _ = 32
   {-# INLINE sizeOf #-}
-  peek = evalStruct $ TSNode <$> peekStruct
-                             <*> peekStruct
-                             <*> peekStruct
-                             <*> peekStruct
-                             <*> peekStruct
+  peek =
+    evalStruct $
+      TSNode
+        <$> peekStruct
+        <*> peekStruct
+        <*> peekStruct
+        <*> peekStruct
+        <*> peekStruct
   {-# INLINE peek #-}
   poke ptr (TSNode sb sp o4 p1 p2) = flip evalStruct ptr $ do
     pokeStruct sb
@@ -127,40 +145,43 @@ instance Storable TSNode where
   {-# INLINE poke #-}
 
 instance Functor Struct where
-  fmap f a = Struct go where
-    go p = do
-      (a', p') <- runStruct a p
-      let fa = f a'
-      fa `seq` p' `seq` pure (fa, castPtr p')
-    {-# INLINE go #-}
+  fmap f a = Struct go
+    where
+      go p = do
+        (a', p') <- runStruct a p
+        let fa = f a'
+        fa `seq` p' `seq` pure (fa, castPtr p')
+      {-# INLINE go #-}
   {-# INLINE fmap #-}
 
 instance Applicative Struct where
-  pure a = Struct (\ p -> pure (a, castPtr p))
+  pure a = Struct (\p -> pure (a, castPtr p))
   {-# INLINE pure #-}
 
-  f <*> a = Struct go where
-    go p = do
-      (f', p')  <- runStruct f          p
-      (a', p'') <- p' `seq` runStruct a (castPtr p')
-      let fa = f' a'
-      fa `seq` p'' `seq` pure (fa, castPtr p'')
-    {-# INLINE go #-}
+  f <*> a = Struct go
+    where
+      go p = do
+        (f', p') <- runStruct f p
+        (a', p'') <- p' `seq` runStruct a (castPtr p')
+        let fa = f' a'
+        fa `seq` p'' `seq` pure (fa, castPtr p'')
+      {-# INLINE go #-}
   {-# INLINE (<*>) #-}
 
 instance Monad Struct where
   return = pure
   {-# INLINE return #-}
 
-  a >>= f = Struct go where
-    go p = do
-      (a', p')   <- runStruct a               p
-      (fa', p'') <- p' `seq` runStruct (f a') (castPtr p')
-      fa' `seq` p'' `seq` pure (fa', p'')
-    {-# INLINE go #-}
+  a >>= f = Struct go
+    where
+      go p = do
+        (a', p') <- runStruct a p
+        (fa', p'') <- p' `seq` runStruct (f a') (castPtr p')
+        fa' `seq` p'' `seq` pure (fa', p'')
+      {-# INLINE go #-}
   {-# INLINE (>>=) #-}
 
-
 foreign import ccall unsafe "src/bridge.c ts_node_copy_child_nodes" ts_node_copy_child_nodes :: Ptr TSNode -> Ptr Node -> IO ()
+
 -- NB: this leaves the field name as NULL.
 foreign import ccall unsafe "src/bridge.c ts_node_poke_p" ts_node_poke_p :: Ptr TSNode -> Ptr Node -> IO ()
--- tree-sitter-simple/src/TreeSitter/Raw/Parser.hs
+++ tree-sitter-simple/src/TreeSitter/Raw/Parser.hs
@@ -1,13 +1,14 @@
 module TreeSitter.Raw.Parser
-( Parser
-, withParser
-, withParseTree
-, ts_parser_new
-, ts_parser_parse_string
-, ts_parser_delete
-, ts_parser_set_language
-, ts_parser_log_to_stderr
-) where
+  ( Parser,
+    withParser,
+    withParseTree,
+    ts_parser_new,
+    ts_parser_parse_string,
+    ts_parser_delete,
+    ts_parser_set_language,
+    ts_parser_log_to_stderr,
+  )
+where
 
 import Control.Exception as Exc
 import Data.ByteString (ByteString)
@@ -26,22 +27,28 @@ withParser :: Ptr Language -> (Ptr Parser -> IO a) -> IO a
 withParser language action = Exc.bracket
   ts_parser_new
   ts_parser_delete
-  $ \ parser -> do
+  $ \parser -> do
     _ <- ts_parser_set_language parser language
     action parser
 
 withParseTree :: Ptr Parser -> ByteString -> (Ptr Tree -> IO a) -> IO a
 withParseTree parser bytestring action =
-  unsafeUseAsCStringLen bytestring $ \ (source, len) -> Exc.bracket
-    (ts_parser_parse_string parser nullPtr source len)
-    releaseTree
-    action
-  where releaseTree t
-          | t == nullPtr = pure ()
-          | otherwise    = ts_tree_delete t
+  unsafeUseAsCStringLen bytestring $ \(source, len) ->
+    Exc.bracket
+      (ts_parser_parse_string parser nullPtr source len)
+      releaseTree
+      action
+  where
+    releaseTree t
+      | t == nullPtr = pure ()
+      | otherwise = ts_tree_delete t
 
 foreign import ccall safe "ts_parser_new" ts_parser_new :: IO (Ptr Parser)
+
 foreign import ccall safe "ts_parser_parse_string" ts_parser_parse_string :: Ptr Parser -> Ptr Tree -> CString -> Int -> IO (Ptr Tree)
+
 foreign import ccall safe "ts_parser_delete" ts_parser_delete :: Ptr Parser -> IO ()
+
 foreign import ccall safe "ts_parser_set_language" ts_parser_set_language :: Ptr Parser -> Ptr Language -> IO Bool
+
 foreign import ccall safe "src/bridge.c ts_parser_log_to_stderr" ts_parser_log_to_stderr :: Ptr Parser -> IO ()
