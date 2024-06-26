5338a8ebd236398b3b19be8b7c4c4772e001aab2
Brian Shu <littlebubu.shu@gmail.com>
2024-06-26 12:04:20 -0400

    rtsopts

--- haskell-ast/app/Main.hs
+++ haskell-ast/app/Main.hs
@@ -2,13 +2,11 @@
 
 module Main where
 
-import AST.Cast qualified as Cast
+import AST qualified
 import AST.Haskell qualified
-import AST.Node qualified as AST
 import AST.Runtime qualified as Runtime
 import Control.DeepSeq qualified as DeepSeq
 import Data.Text.IO qualified as T.IO
-import NeatInterpolation
 import System.Environment (getArgs)
 import Text.Pretty.Simple
 import TreeSitter.Api
@@ -19,9 +17,8 @@ main = do
   [file] <- getArgs
   !tree <- parse tree_sitter_haskell <$> T.IO.readFile file
   let childDesc = Runtime.getChildDescription tree
-  let !(typedTree :: Maybe AST.Haskell.Haskell) = Cast.cast tree
+  let !(typedTree :: Maybe AST.Haskell.Haskell) = AST.cast tree
   pPrintOpt NoCheckColorTty (defaultOutputOptionsDarkBg {outputOptionsIndentAmount = 1}) tree
   pPrintOpt NoCheckColorTty (defaultOutputOptionsDarkBg {outputOptionsIndentAmount = 1}) typedTree
   let !_ = DeepSeq.rnf typedTree
   pure ()
-  
