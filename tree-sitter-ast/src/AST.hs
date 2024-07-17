3a1a046f94f8ee7120e66f7d503bf193c8037267
Brian Shu <littlebubu.shu@gmail.com>
2024-07-17 13:48:23 -0400

    reexport unwrapped class

--- tree-sitter-ast/src/AST.hs
+++ tree-sitter-ast/src/AST.hs
@@ -6,4 +6,5 @@ import AST.Node as X
 import AST.Sum as X
 import AST.Token as X
 import AST.Traversal as X
-import TreeSitter.Api as X (ConvertPos, FullDynNode(..))
+import AST.Unwrap as X
+import TreeSitter.Api as X (ConvertPos, FullDynNode (..))
